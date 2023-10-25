fa4c282ceffca7e41f84a14d28e49631ffb05cd2
ben lim <lim6112j@gmail.com>
2023-10-25 16:48:26 +0900

    ...

--- repositories/OSRMRepository.fs
+++ repositories/OSRMRepository.fs
@@ -25,7 +25,21 @@ module OSRMRepository =
         >=> Writers.setMimeType "application/json"
 
 
-
+    /// <summary>
+    /// request body
+    ///     {
+    ///     "waypoints": [
+    ///           { "Lng" : "126.79939689052419", "Lat" : "37.527319039426736" },
+    ///           { "Lng" : "126.87491792408139","Lat" : "37.627320777159646" },
+    ///           { "Lng" : "127.06568457951413","Lat" : "37.665543301893806" },
+    ///           { "Lng" : "127.16568457951413", "Lat" : "37.565543301893806" }
+    ///     ],
+    ///     "demands": [
+    ///           { "Lng" : "126.80939689052419", "Lat" : "37.547319039426736" },
+    ///           { "Lng" : "126.90491792408139", "Lat" : "37.657320777159646" }
+    ///     ]
+    /// }
+    /// </summary>
     let apiPostCall (param: OsrmReq) =
 
         let waypoints = param.waypoints
